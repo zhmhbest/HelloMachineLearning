84ee3e4af4fb40bb90faa5843003412f7f970128
zhmhbest <zhmhbest@gmail.com>
2020-08-12 22:14:20 +0800

    update

--- notes/keras/src/demo-lstm.py
+++ notes/keras/src/demo-lstm.py
@@ -8,7 +8,7 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM
 from keras.losses import mean_squared_error as keras_mse
 from keras.optimizers import adam as keras_adam
-from sklearn. preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 
 
@@ -16,7 +16,7 @@ from sklearn.metrics import mean_squared_error
     模型定义与编译
 """
 model = Sequential()
-model.add(LSTM(10, input_shape=()))
+model.add(LSTM(units=10, input_shape=()))
 model.add(Dense(1))
-model.compile(loss=keras_mse, optimizer=keras_adagrad)
+model.compile(loss=keras_mse, optimizer=keras_adam)
 
