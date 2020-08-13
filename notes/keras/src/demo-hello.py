3f74bf8cee2994229c7812a13707b4dff474096f
zhmhbest <zhmhbest@gmail.com>
2020-08-14 00:00:23 +0800

    hello keras

--- notes/keras/src/demo-hello.py
+++ notes/keras/src/demo-hello.py
@@ -1,62 +1,75 @@
 import numpy as np
 from keras.models import Sequential
-from zhmh.dataset import generate_random_data
-from keras.layers.core import (Dense, Dropout, Activation)
-from keras.losses import mean_squared_error
-from keras.optimizers import adam
-from sklearn.model_selection import train_test_split
+from keras.models import (load_model, model_from_json)
+from keras.layers.core import (Dense, Activation)
+from keras.optimizers import sgd
 from matplotlib import pyplot
+import os
+DUMP_PATH = './dump'
+if not os.path.exists(DUMP_PATH):
+    os.makedirs(DUMP_PATH)
+MODEL_FILE = f"{DUMP_PATH}/model_hello.h5"
 
 
 """
     模拟数据
 """
-data_x, data_y = generate_random_data(512, 2, 1)
-x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=0.2)
-
-"""
-    模型定义
-"""
-model = Sequential()
-model.add(Dense(4, input_dim=x_train.shape[1]))
-model.add(Activation('elu'))
-model.add(Dense(2, input_dim=x_train.shape[1]))
-model.add(Activation('elu'))
-model.add(Dense(1))
-
-
-"""
-    编译模型
-"""
-model.compile(
-    loss=mean_squared_error,
-    optimizer=adam(lr=0.01)
+x_data = np.random.rand(128).reshape(-1, 1)
+y_data = np.random.normal(
+    (lambda x: np.sqrt(-x ** 2 + x * 0.5 + 0.8))(x_data),
+    0.008
 )
 
 
-"""
-    模型训练
-"""
-model.fit(
-    x_train, y_train,
-    batch_size=32,      # 每组数量
-    epochs=50,         # 循环次数
-    validation_data=(x_test, y_test),
-)
+if os.path.exists(MODEL_FILE):
+    """
+        模型加载
+    """
+    model = load_model(MODEL_FILE)
+else:
+    """
+        模型定义与编译（首次）
+    """
+    model = Sequential()
+    model.add(Dense(8, input_dim=x_data.shape[1]))
+    model.add(Activation('tanh'))
+    model.add(Dense(4))
+    model.add(Activation('tanh'))
+    model.add(Dense(1))
+    model.add(Activation('tanh'))
+    model.compile(
+        loss='mse',
+        optimizer=sgd(lr=0.1)
+    )
+
+    """
+        模型训练（首次）
+    """
+    for step in range(2000):
+        loss_val = model.train_on_batch(x_data, y_data)
+        if 0 == step % 200:
+            print('loss:', loss_val)
+
+    """
+        模型评估与保存（首次）
+    """
+    score = model.evaluate(x_data, y_data, verbose=0)
+    print("score[loss] =", score)
+    model.save(MODEL_FILE)
 
 
 """
-    模型评估
+    打印模型结构
 """
-score = model.evaluate(x_test, y_test, verbose=0)
-print("score[loss] =", score)
+model.summary()
 
 
 """
     模型预测
 """
-test_pred = model.predict_classes(x_test)
-pyplot.plot(test_pred)
-pyplot.plot(y_test)
+y_pred = model.predict(x_data)
+pyplot.scatter(x_data, y_data, label='data')
+pyplot.scatter(x_data, y_pred, label='pred')
+pyplot.grid()
+pyplot.legend()
 pyplot.show()
-
