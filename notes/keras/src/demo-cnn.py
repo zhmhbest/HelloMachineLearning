7ef40417d545c618fa6d290d3d0ad31b3172a36c
zhmhbest <zhmhbest@gmail.com>
2020-08-23 19:37:47 +0800

    CNN

--- notes/keras/src/demo-cnn.py
+++ notes/keras/src/demo-cnn.py
@@ -10,7 +10,7 @@ from keras.activations import relu, softmax
 from keras.losses import categorical_crossentropy
 from keras.metrics import categorical_accuracy
 from keras.models import Sequential, load_model
-from keras.constraints import maxnorm
+from keras.constraints import MaxNorm
 from keras.layers.core import (Dense, Dropout, Activation)
 """
     Dense           : 用于创建密集神经元
@@ -18,39 +18,61 @@ from keras.layers.core import (Dense, Dropout, Activation)
     Activation      : 用于创建激活层
 """
 
-from keras.layers import (Conv2D, MaxPooling2D, Flatten)
+from keras.layers import (
+    Conv2D,
+    MaxPooling2D, AveragePooling2D,
+    Flatten
+)
 """
-    Conv2D          : 卷积层
-        filters         : {int} 过滤器输出深度
-        kernel_size     : {int | (int, int)} 过滤器窗口尺寸
-        strides         : {int | (int, int)} 过滤器过滤步长
-        padding         : {'valid' | 'same'} 过滤器模式
+    【卷积层】
+    Conv2D
+        filters             : {int} 过滤器输出深度
+        kernel_size         : {int | (int, int)} 过滤器窗口尺寸
+        strides             : {int | (int, int)} 过滤器过滤步长
+        padding             : {'valid' | 'same'} 过滤器模式
             valid = 无填充
             same  = 填充输入，使输出与原始输入的长度相同
-        data_format     : {'channels_last' | 'channels_first'} 数据格式
-            channels_last , input_shape=(samples, rows, cols, channels)
-            channels_first, input_shape=(samples, channels, rows, cols)
-        dilation_rate   : {int | (int, int)} 过滤器扩展
-        activation      : {str | keras.activations.?} 激活函数
-        use_bias        : {bool} 是否使用偏置
+        data_format         : {'channels_last' | 'channels_first'} 数据格式
+            channels_last
+                input_shape=(samples, rows, cols, channels)
+                output_shape=(samples, new_rows, new_cols, filters)
+            channels_first
+                input_shape=(samples, channels, rows, cols)
+                output_shape=(samples, filters, new_rows, new_cols)
+        dilation_rate       : {int | (int, int)} 过滤器扩展
+        activation          : {str | keras.activations.?} 激活函数
+        use_bias            : {bool} 是否使用偏置
+        kernel_constraint   : {keras.constraints.?} 约束主权重矩阵
+            ?=MaxNorm       最大范数权值约束
+            ?=NonNeg        权重非负的约束
+            ?=UnitNorm      映射到每个隐藏单元的权值的约束，使其具有单位范数
+            ?=MinMaxNorm    最小/最大范数权值约束
+        bias_constraint     : {keras.constraints.?} 约束偏置
 
-    MaxPooling2D    : 池化层
+    【池化层】
+    MaxPooling2D/AveragePooling2D
         pool_size       : {int | (int, int)} 池化窗口尺寸
         strides         : {int | (int, int)} 池化步长
         padding         : {'valid' | 'same'} 池化模式
         data_format     : {'channels_last' | 'channels_first'} 数据格式
-            channels_last , input_shape=(batch_size, rows, cols, channels)
-            channels_first, input_shape=(batch_size, channels, rows, cols)
-
-    Flatten         : 多维的输入一维化，相当于reshape((-1, ))
-        input_shape=(None, ..., ...., ...)
-        output_shape=(None, ...)
+            channels_last
+                input_shape=(batch_size, rows, cols, channels)
+                output_shape=(batch_size, pooled_rows, pooled_cols, channels)
+            channels_first
+                input_shape=(batch_size, channels, rows, cols)
+                output_shape=(batch_size, channels, pooled_rows, pooled_cols)
+    
+    【扁平层】
+    Flatten         : 特征一维化
+        input_shape=(None, n1, n2, ...)
+        output_shape=(None, n1 * n2 * ...)
 """
 
 DUMP_PATH = './dump'
 if not os.path.exists(DUMP_PATH):
     os.makedirs(DUMP_PATH)
 MODEL_FILE = f"{DUMP_PATH}/model_cnn.h5"
+CONTINUE_TRAIN = True
 
 
 # 【数据载入】
@@ -100,20 +122,28 @@ if os.path.exists(MODEL_FILE):
     # 【模型加载】
     # ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■
     model = load_model(MODEL_FILE)
+    print(f'Load "{MODEL_FILE}"')
 else:
     # 【模型定义（首次）】
     # ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■
+    print('Define model')
+    # 准确率最高可达 65%
     model = Sequential()
-    model.add(Conv2D(16, (3, 3), input_shape=INPUT_SHAPE, padding='same', activation=relu, kernel_constraint=maxnorm(3)))
-    model.add(Conv2D(16, (3, 3), activation=relu, padding='same', kernel_constraint=maxnorm(3)))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(8, (2, 2), input_shape=INPUT_SHAPE, padding='same', activation=relu, kernel_constraint=MaxNorm(3)))
+    model.add(Conv2D(16, (3, 3), activation=relu, padding='same', kernel_constraint=MaxNorm(3)))
+    model.add(Conv2D(32, (4, 4), activation=relu, padding='same', kernel_constraint=MaxNorm(3)))
+    model.add(AveragePooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (2, 2), activation=relu, padding='same', kernel_constraint=MaxNorm(3)))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
-    model.add(Dense(128, activation=relu, kernel_constraint=maxnorm(3)))
+    model.add(Dense(32))
+    model.add(Activation(relu))
     model.add(Dropout(0.2))
-    model.add(Dense(32, activation=relu, kernel_constraint=maxnorm(3)))
+    model.add(Dense(16))
+    model.add(Activation(relu))
     model.add(Dropout(0.2))
-    model.add(Dense(10, activation=softmax))
+    model.add(Dense(10))
+    model.add(Activation(softmax))
 
     # 【模型编译（首次）】
     # ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■
@@ -124,21 +154,25 @@ else:
         metrics=[categorical_accuracy]
     )
 
-    # 【模型训练（首次）】
+
+if CONTINUE_TRAIN:
+    # 【模型训练】
     # ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■
     model.fit(
         X_train, Y_train,
         batch_size=1000,                    # 每组数量
-        epochs=20,                          # 循环次数
+        epochs=10,                          # 循环次数
         validation_data=(X_test, Y_test),
+        verbose=2,
         callbacks=[
-            EarlyStopping(patience=2),      # 出现梯度爆炸或消失时停止训练
+            EarlyStopping(patience=2, verbose=2),   # 出现梯度爆炸或消失时停止训练
         ]
     )
 
-    # 【保存模型（首次）】
+    # 【保存模型】
     # ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■ ■■■■■■■■■■■■■■■■
     model.save(MODEL_FILE)
+    print(f'Saved to "{MODEL_FILE}"')
 
 
 # 【模型评估】
